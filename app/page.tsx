df6079bec779fa09c10b7b78a57dd8352f5d1e77
dascarlet <nisigin@gmail.com>
2025-03-10 00:46:10 +0900

    Move copy notification below button

--- app/page.tsx
+++ app/page.tsx
@@ -275,7 +275,7 @@ export default function Home() {
                     Copy Text
                   </button>
                   {showCopyNotification && (
-                    <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-1 bg-gray-800 text-white text-sm rounded shadow-lg whitespace-nowrap">
+                    <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 px-3 py-1 bg-gray-800 text-white text-sm rounded shadow-lg whitespace-nowrap">
                       Copied to clipboard!
                     </div>
                   )}
